9dd1b29b497f4585d624ca359b1abc4b6c2ce1ff
xiaojingya1 <xiaojingya1@jd.com>
2019-08-29 16:03:29 +0800

    Enter c++ mode for .h file

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 (setq-default c-basic-offset 4) ; set 4 spaces as indent
 (setq-default indent-tabs-mode nil) ; disable tabs
 (c-set-offset 'innamespace 0) ; disable indent in namespace keyword
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)) ; enter c++ mode for .h file
 ; indent 2 times of basic offset in arglist
 ; (setq c-offsets-alist '((arglist-cont-nonempty . ++)
 ; 			(arglist-intro . ++)
