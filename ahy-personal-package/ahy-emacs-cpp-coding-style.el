8ce1ddbca1a598e1e2b18cd8236786535e12ccb1
anhongyu1 <anhongyu@jd.com>
2019-09-10 20:52:33 +0800

    Set c offset in one place

--- ahy-personal-package/ahy-emacs-cpp-coding-style.el
+++ ahy-personal-package/ahy-emacs-cpp-coding-style.el
@@ -8,11 +8,11 @@
 
 (setq-default c-basic-offset 4) ; set 4 spaces as indent
 (setq-default indent-tabs-mode nil) ; disable tabs
-(c-set-offset 'innamespace 0) ; disable indent in namespace keyword
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)) ; enter c++ mode for .h file
 ; indent 2 times of basic offset in arglist
 ;; (setq c-offsets-alist '((arglist-cont-nonempty . ++)
 ;;                         (arglist-intro . ++)
 ;;                         (arglist-close . ++)))
-(setq c-offsets-alist '((arglist-intro . ++)))
+(setq c-offsets-alist '((innamespace . 0)
+                        (arglist-intro . ++)))
 (add-hook 'before-save-hook 'delete-trailing-whitespace) ; clean up the whitespaces in the end of line
