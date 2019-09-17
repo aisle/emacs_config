3aea86c86b5ea145abb29aed7e2257e031c2abd4
anhongyu1 <anhongyu@jd.com>
2019-09-17 20:54:04 +0800

    Improve indentation of continued C++11 lambda function

--- ahy-personal-package/ahy-emacs-cpp-coding-style.el
+++ ahy-personal-package/ahy-emacs-cpp-coding-style.el
@@ -16,3 +16,16 @@
 (setq c-offsets-alist '((innamespace . 0)
                         (arglist-intro . ++)))
 (add-hook 'before-save-hook 'delete-trailing-whitespace) ; clean up the whitespaces in the end of line
+
+(defadvice c-lineup-arglist (around my activate)
+  "Improve indentation of continued C++11 lambda function opened as argument."
+  (setq ad-return-value
+        (if (and (equal major-mode 'c++-mode)
+                 (ignore-errors
+                   (save-excursion
+                     (goto-char (c-langelem-pos langelem))
+                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+                     ;;   and with unclosed brace.
+                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+            0                           ; no additional indent
+          ad-do-it)))                   ; default behavior
