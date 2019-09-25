2f5e3e5f8b62c55af0beda1706ba5dd203fbb0e8
anhongyu1 <anhongyu@jd.com>
2019-09-25 17:35:12 +0800

    Add unbound plugin to get available key combinations

--- ahy-personal-package/ahy-emacs-plugins-manager.el
+++ ahy-personal-package/ahy-emacs-plugins-manager.el
@@ -16,6 +16,10 @@
              (concat emacs-config-root "manual-packages/auto-complete-master/dict"))
 (ac-config-default)
 
+;; Enable unbound plugin, to check available key combinations
+;; Usage: M-x describe-unbound-keys
+(require 'unbound)
+
 ;; Enable protobuf-mode
 (require 'protobuf-mode)
 
