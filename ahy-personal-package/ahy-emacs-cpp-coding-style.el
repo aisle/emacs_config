ec18054280ec9247a55767ec5d031e7e94d22086
anhongyu1 <anhongyu@jd.com>
2019-09-25 15:11:19 +0800

    Shift left & shift right current line

--- ahy-personal-package/ahy-emacs-cpp-coding-style.el
+++ ahy-personal-package/ahy-emacs-cpp-coding-style.el
@@ -17,6 +17,20 @@
                         (arglist-intro . ++)))
 (add-hook 'before-save-hook 'delete-trailing-whitespace) ; clean up the whitespaces in the end of line
 
+(defun shift-right-current-line ()
+  "Shift right current line, by basic offset"
+  (interactive)
+  (let ((start (point-at-bol))
+        (end (point-at-eol)))
+    (indent-rigidly start end c-basic-offset)))
+
+(defun shift-left-current-line ()
+  "Shift left current line, by basic offset"
+  (interactive)
+  (let ((start (point-at-bol))
+        (end (point-at-eol)))
+    (indent-rigidly start end (- c-basic-offset))))
+
 (defadvice c-lineup-arglist (around my activate)
   "Improve indentation of continued C++11 lambda function opened as argument."
   (setq ad-return-value
